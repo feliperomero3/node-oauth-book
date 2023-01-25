f773a0ab05346d31a94400617dc9529cd2259f54
Felipe Romero <feliperomero3@outlook.com>
2023-01-24 22:09:11 -0600

    Add underscore view engine.

--- src/authorization-server/app.js
+++ src/authorization-server/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const https = require('https');
 const path = require('path');
+var cons = require('consolidate');
+var __ = require('underscore');
+__.string = require('underscore.string');
 
 const options = {
   key: fs.readFileSync(path.join(__dirname, '../server.key')),
@@ -12,17 +15,23 @@ const options = {
 var app = express();
 
 app.set('appname', 'authorization-server');
+app.set('json spaces', 2);
 app.set('port', process.env.PORT || 9001);
 app.set('host', process.env.HOST || 'localhost');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+app.engine('html', cons.underscore);
+
+app.use(express.static('views'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = https.createServer(options, (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+app.get('/', (req, res) => {
+  res.render('index');
 });
+
+const server = https.createServer(options, app);
 
 server.listen(app.get('port'), app.get('host'), () => {
   var host = server.address().address;
